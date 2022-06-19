851aad00e14f34d5ee07cd3d282324334564465f
Jay Hung <37239758+JayChung0258@users.noreply.github.com>
2022-06-19 20:51:18 +0800

    Update test
    Complete the deploy, metadata test.

--- test/KryptoBird.test.js
+++ test/KryptoBird.test.js
@@ -1,5 +1,4 @@
-const assert = require("chai");
-const { Item } = require("react-bootstrap/lib/Breadcrumb");
+const { assert } = require("chai");
 
 const KryptoBird = artifacts.require("./KryptoBird");
 
@@ -12,15 +11,25 @@ contract("KryptoBird", (account) => {
 
   // testing container - describe
 
-  decribe("deployment", async () => {
+  describe("deployment", async () => {
     // testing samples with writing it
     it("deploys successfully", async () => {
       contract = await KryptoBird.deployed();
-      const address = contrat.address;
+      const address = contract.address;
       assert.notEqual(address, "");
       assert.notEqual(address, null);
       assert.notEqual(address, undefined);
       assert.notEqual(address, 0x0);
+    });
+
+    it("correct name", async () => {
+      const name = await contract.name();
+      assert.equal(name, "KryptoBird");
+    });
+
+    it("correct symbol", async () => {
+      const symbol = await contract.symbol();
+      assert.equal(symbol, "JAY");
     });
   });
 });
