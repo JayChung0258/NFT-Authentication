392238ddad4f06d2d79ce325ed6e9dce886c1ec8
Jay Hung <37239758+JayChung0258@users.noreply.github.com>
2022-10-03 03:38:30 +0800

    1003
    Complete data get from firebase and complete card filter.

--- src/components/Contract.js
+++ src/components/Contract.js
@@ -1,32 +1,70 @@
 import React, { Component } from "react";
 import Web3 from "web3";
-import { ref, set } from "firebase/database";
+import {
+  getDatabase,
+  child,
+  get,
+  ref,
+  set,
+  orderByChild,
+  query,
+} from "firebase/database";
 import database from "./utils/firebase";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
+
+import FormLabel from "@mui/material/FormLabel";
+import FormControl from "@mui/material/FormControl";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormHelperText from "@mui/material/FormHelperText";
+import Switch from "@mui/material/Switch";
+
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCardImage,
+  MDBBtn,
+} from "mdb-react-ui-kit";
+import "./DashBoard.css";
 
 class Contract extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      // env data
       db: null,
       account: "",
       contract: null,
+
+      // input data
+      contractName: "",
+      contractNFTs: "",
+      contractEmail: false,
+      contractPhone: false,
+      contractBirthday: false,
+      contractAddress: "",
+
+      // series data
+      NFTsData: {},
     };
 
     // methods binding
   }
 
   async componentDidMount() {
-    // await this.loadFireBase();
     this.setState({ db: database });
-    // this.writeUserData("1", "Jay", "jaychung@gmail.com");
 
     const web3Exist = await this.checkWeb3Exists();
     if (web3Exist) {
       await this.loadWeb3();
       await this.loadBlockchainData();
     }
+
+    // get data from firebase when loaded
+    this.getDB();
   }
 
   // check if web3 is installed
@@ -66,26 +104,54 @@ class Contract extends Component {
     console.log("Account: " + this.state.account);
   }
 
-  writeUserData(userId, name, email) {
+  writeDB() {
     console.log("write called!");
     console.log(database);
-    set(ref(database, "Contract/" + userId), {
-      username: name,
-      email: email,
+
+    set(ref(database, "Contracts/" + this.state.contractName), {
+      contractName: this.state.contractName,
+      contractOwnerAddress: this.state.account,
+      contractNFTs: this.state.contractNFTs,
+      contractEmail: this.state.contractEmail,
+      contractPhone: this.state.contractPhone,
+      contractBirthday: this.state.contractBirthday,
     })
       .then(() => {
         alert("write success!");
+        this.getDB();
       })
       .catch((error) => {
         alert("write failed!");
       });
   }
 
-  //   async loadFireBase() {
-  //     console.log("loadFireBase called!");
-  //     const database = getDatabase(useFireBase);
-  //     this.setState({ database });
-  //   }
+  getDB() {
+    const dbRef = ref(database);
+    get(child(dbRef, "Contracts"))
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          console.log(snapshot.val());
+          this.setState({ NFTsData: snapshot.val() });
+        } else {
+          console.log("No data available");
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  getSortNFTsData() {
+    const NFTsData = this.state.NFTsData;
+    const NFTsDataArray = [];
+    for (let key in NFTsData) {
+      if (NFTsData[key].contractOwnerAddress == this.state.account) {
+        NFTsDataArray.push(NFTsData[key]);
+      }
+    }
+    console.log(NFTsDataArray);
+    return NFTsDataArray;
+  }
 
   render() {
     return (
@@ -114,159 +180,113 @@ class Contract extends Component {
           <div>
             <TextField
               required
-              id="outlined-required"
-              label="Required"
-              defaultValue="Hello World"
+              id="outlined-name"
+              label="Contract Name"
+              onChange={(e) => this.setState({ contractName: e.target.value })}
             />
             <TextField
+              fullWidth
               disabled
-              id="outlined-disabled"
-              label="Disabled"
-              defaultValue="Hello World"
+              id="outlined-address"
+              label="Address of creater"
+              value={this.state.account}
             />
             <TextField
-              id="outlined-password-input"
-              label="Password"
-              type="password"
-              autoComplete="current-password"
-            />
-            <TextField
-              id="outlined-read-only-input"
-              label="Read Only"
-              defaultValue="Hello World"
-              InputProps={{
-                readOnly: true,
-              }}
-            />
-            <TextField
+              required
               id="outlined-number"
-              label="Number"
+              label="Number of NFTs"
               type="number"
               InputLabelProps={{
                 shrink: true,
               }}
-            />
-            <TextField
-              id="outlined-search"
-              label="Search field"
-              type="search"
-            />
-            <TextField
-              id="outlined-helperText"
-              label="Helper text"
-              defaultValue="Default Value"
-              helperText="Some important text"
+              onChange={(e) => this.setState({ contractNFTs: e.target.value })}
             />
           </div>
           <div>
-            <TextField
-              required
-              id="filled-required"
-              label="Required"
-              defaultValue="Hello World"
-              variant="filled"
-            />
-            <TextField
-              disabled
-              id="filled-disabled"
-              label="Disabled"
-              defaultValue="Hello World"
-              variant="filled"
-            />
-            <TextField
-              id="filled-password-input"
-              label="Password"
-              type="password"
-              autoComplete="current-password"
-              variant="filled"
-            />
-            <TextField
-              id="filled-read-only-input"
-              label="Read Only"
-              defaultValue="Hello World"
-              InputProps={{
-                readOnly: true,
-              }}
-              variant="filled"
-            />
-            <TextField
-              id="filled-number"
-              label="Number"
-              type="number"
-              InputLabelProps={{
-                shrink: true,
-              }}
-              variant="filled"
-            />
-            <TextField
-              id="filled-search"
-              label="Search field"
-              type="search"
-              variant="filled"
-            />
-            <TextField
-              id="filled-helperText"
-              label="Helper text"
-              defaultValue="Default Value"
-              helperText="Some important text"
-              variant="filled"
-            />
-          </div>
-          <div>
-            <TextField
-              required
-              id="standard-required"
-              label="Required"
-              defaultValue="Hello World"
-              variant="standard"
-            />
-            <TextField
-              disabled
-              id="standard-disabled"
-              label="Disabled"
-              defaultValue="Hello World"
-              variant="standard"
-            />
-            <TextField
-              id="standard-password-input"
-              label="Password"
-              type="password"
-              autoComplete="current-password"
-              variant="standard"
-            />
-            <TextField
-              id="standard-read-only-input"
-              label="Read Only"
-              defaultValue="Hello World"
-              InputProps={{
-                readOnly: true,
-              }}
-              variant="standard"
-            />
-            <TextField
-              id="standard-number"
-              label="Number"
-              type="number"
-              InputLabelProps={{
-                shrink: true,
-              }}
-              variant="standard"
-            />
-            <TextField
-              id="standard-search"
-              label="Search field"
-              type="search"
-              variant="standard"
-            />
-            <TextField
-              id="standard-helperText"
-              label="Helper text"
-              defaultValue="Default Value"
-              helperText="Some important text"
-              variant="standard"
-            />
+            <FormControl component="fieldset" variant="standard">
+              <FormLabel component="legend">Assign responsibility</FormLabel>
+              <FormGroup>
+                <FormControlLabel
+                  control={
+                    <Switch
+                      checked={this.state.contractEmail}
+                      onChange={(e) =>
+                        this.setState({ contractEmail: e.target.checked })
+                      }
+                      name="email"
+                    />
+                  }
+                  label="Email"
+                />
+                <FormControlLabel
+                  control={
+                    <Switch
+                      checked={this.state.contractPhone}
+                      onChange={(e) =>
+                        this.setState({ contractPhone: e.target.checked })
+                      }
+                      name="phone"
+                    />
+                  }
+                  label="Phone"
+                />
+                <FormControlLabel
+                  control={
+                    <Switch
+                      checked={this.state.contractBirthday}
+                      onChange={(e) =>
+                        this.setState({ contractBirthday: e.target.checked })
+                      }
+                      name="birthday"
+                    />
+                  }
+                  label="Birthday"
+                />
+              </FormGroup>
+              <FormHelperText>
+                Be sure to request more data from users
+              </FormHelperText>
+            </FormControl>
           </div>
         </Box>
+        <button
+          type="button"
+          class="btn btn-primary"
+          onClick={() => this.writeDB()}
+        >
+          Create
+        </button>
+        <button
+          type="button"
+          class="btn btn-primary"
+          onClick={() => this.getDB()}
+        >
+          Get
+        </button>
+        <div>
+          {this.getSortNFTsData().map((data, key) => {
+            return (
+              <div>
+                <div>
+                  <MDBCard className="token img" style={{ maxWidth: "22rem" }}>
+                    <MDBCardBody>
+                      <MDBCardTitle></MDBCardTitle>
+                      <MDBCardText>
+                        Contract Name : {data.contractName}
+                      </MDBCardText>
+                      <MDBCardText>
+                        Number of NFTs : {data.contractNFTs}
+                      </MDBCardText>
+                      <MDBCardText>
+                        contractOwnerAddress : {data.contractOwnerAddress}
+                      </MDBCardText>
+                    </MDBCardBody>
+                  </MDBCard>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   }
---
+++ src/components/codeMemory.js
@@ -0,0 +1,25 @@
+{
+  /* <TextField
+              id="filled-email"
+              label="Email"
+              helperText="additional info"
+              variant="filled"
+              onChange={(e) => this.setState({ contractEmail: e.target.value })}
+            />
+            <TextField
+              id="filled-phone"
+              label="Phone"
+              helperText="additional info"
+              variant="filled"
+              onChange={(e) => this.setState({ contractPhone: e.target.value })}
+            />
+            <TextField
+              id="filled-birthday"
+              label="birthday"
+              helperText="additional info"
+              variant="filled"
+              onChange={(e) =>
+                this.setState({ contractBirthday: e.target.value })
+              }
+            /> */
+}
